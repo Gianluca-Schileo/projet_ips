02585cf89d4f4c9ca88f2cee3aecabdb363b9ced
Gianluca <github.b1ewl@simplelogin.com>
2023-05-04 17:30:58 +0200

    Delete environement.sh

--- environement.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-conda create --name Projet_IPS python=3.11
-conda activate Projet_IPS
-
-conda install Jupiter -y
-conda install pandas -y
-conda install matplotlib -y
-conda install plotly -y
