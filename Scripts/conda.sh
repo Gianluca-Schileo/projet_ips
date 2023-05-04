0ddd10cee95d45f6b27c826e333fd857a9d0e7f9
Gianluca <github.b1ewl@simplelogin.com>
2023-05-04 17:29:03 +0200

    Create conda.sh

---
+++ Scripts/conda.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+conda create --name Projet_IPS python=3.11
+conda activate Projet_IPS
+
+conda install Jupiter -y
+conda install pandas -y
+conda install matplotlib -y
+conda install plotly -y
